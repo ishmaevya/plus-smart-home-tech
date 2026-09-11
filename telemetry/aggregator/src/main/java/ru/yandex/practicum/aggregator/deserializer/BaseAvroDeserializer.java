a36257aca2c9e687b23e7a3651786f008f65e260
Yaroslav Ishmaev <ishmaev.slava@yandex.ru>
2026-09-11 18:29:41 +0300

    Добавлен сервис Aggregator + схема снапшотов

---
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/aggregator/deserializer/BaseAvroDeserializer.java
@@ -0,0 +1,40 @@
+package ru.yandex.practicum.aggregator.deserializer;
+
+import org.apache.avro.Schema;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.Decoder;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.common.serialization.Deserializer;
+
+import java.io.IOException;
+
+public abstract class BaseAvroDeserializer<T extends SpecificRecordBase> implements Deserializer<T> {
+
+    private final Schema schema;
+    private final DecoderFactory decoderFactory;
+
+    protected BaseAvroDeserializer(Schema schema) {
+        this(DecoderFactory.get(), schema);
+    }
+
+    protected BaseAvroDeserializer(DecoderFactory decoderFactory, Schema schema) {
+        this.decoderFactory = decoderFactory;
+        this.schema = schema;
+    }
+
+    @Override
+    public T deserialize(String topic, byte[] data) {
+        if (data == null) {
+            return null;
+        }
+        DatumReader<T> reader = new SpecificDatumReader<>(schema);
+        try {
+            Decoder decoder = decoderFactory.binaryDecoder(data, null);
+            return reader.read(null, decoder);
+        } catch (IOException e) {
+            throw new RuntimeException("Ошибка десериализации Avro: " + e.getMessage(), e);
+        }
+    }
+}
